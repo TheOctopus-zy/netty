3e98b71756c91e579adfa231e5709045588814d7
‘TheOctopus <zhangyu.techlife@gmail.com>
2020-04-12 18:37:34 +0800

    add by zhangyu

--- src/main/java/nio/Demo.java
+++ src/main/java/nio/Demo.java
@@ -1,7 +1,16 @@
 package nio;
 
+import java.nio.MappedByteBuffer;
+
 /**
  * Date: 2020/4/12
  **/
 public class Demo {
+  public static void main(String[] args){
+     // MappedByteBuffer
+  }
+
+
 }
+
+
---
+++ src/main/java/nio/channel/MappedByteBufferTest.java
@@ -0,0 +1,23 @@
+package nio.channel;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * MappedByteBuffer  可以让文件直接在内存中修改(堆外内存)，操作系统不需要拷贝一次
+ **/
+public class MappedByteBufferTest {
+
+    public static void main(String[] args) throws IOException {
+        //创建一个文件操作IO对象
+        RandomAccessFile accessFile = new RandomAccessFile("a.txt", "rw");
+        //获取channel对象
+        FileChannel channel = accessFile.getChannel();
+        MappedByteBuffer map = channel.map(FileChannel.MapMode.READ_WRITE, 0, 5);
+        map.put(0, (byte) 'A');
+        map.put(2, (byte) 'B');
+        channel.close();
+    }
+}
